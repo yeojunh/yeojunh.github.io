fc61e2acd91153ed632fe4bd12ade1f5c3b88f9c
yeojunh <yeojunhann@gmail.com>
2024-09-10 23:26:46 -0700

    Updates

--- threejs-graphics-project/glsl/skybox.fs.glsl
+++
@@ -1,8 +0,0 @@
-// The cubmap texture is of type SamplerCube
-
-uniform samplerCube skybox;
-in vec3 wcsPosition;
-
-void main() {
-	gl_FragColor = texture(skybox, wcsPosition);
-}
--- threejs-graphics-project/glsl/skybox.vs.glsl
+++
@@ -1,7 +0,0 @@
-uniform vec3 cameraPos;
-out vec3 wcsPosition;
-
-void main() {
-	wcsPosition = vec3(modelMatrix * vec4(position, 1.0));
-	gl_Position = projectionMatrix * viewMatrix * vec4(position, 1.0);
-}
